a5f74f56e8d4e76c40555eb24ef4ed8a421af6f1
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2026-07-14 10:27:09 +0900

    feat: add nvim plugin gitsigns

---
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,17 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    signs = {
+      add          = { text = "│" },
+      change       = { text = "│" },
+      delete       = { text = "_" },
+      topdelete    = { text = "‾" },
+      changedelete = { text = "~" },
+    },
+    current_line_blame = false,
+    signcolumn = true,
+    numhl = false,
+    linehl = false,
+  },
+}
